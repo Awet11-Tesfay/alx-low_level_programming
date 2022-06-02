f55727fdcdac790d88915b0848d272801eca6872
Awet11_Tesfay <hailegb2021@gmail.com>
2022-06-02 16:06:02 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,11 @@
-#include <stdio.h>
+#include <unistd.h>
 /**
-*main - writes out the values
+*main - Entry point
 *
-*Description: avails the tru values out.
-*Return: prints out 1
+*Return: 1 
 */
 int main(void)
-{ FILE * fp;
-char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-fp = fopen("file.txt", "w");
-fwrite(str, 2, sizeof(str), fp);
-fclose(fp);
+{
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
