4aef844da4e42502f666a3984f17e75b61bf3f5d
Awet11_Tesfay <hailegb2021@gmail.com>
2022-06-02 14:17:21 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 /**
  * main - Entry point
@@ -7,6 +6,7 @@
  */
  int main(void)
 {
- puts("\"Programming is like building a multilingual puzzle");
- return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
+
