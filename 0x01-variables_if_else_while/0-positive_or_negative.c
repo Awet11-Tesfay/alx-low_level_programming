60616d1154cca41b89b863c75b44b6146a50276b
Awet11_Tesfay <hailegb2021@gmail.com>
2022-06-03 11:32:07 -0700

    Tsk 0

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,23 @@
+#include <stdio.h> 
+/**
+*main-always printing integer
+*Return:always 0.
+*/
+int main(void)
+int num;
+printf("please enter your number: ");
+scanf ("%d", &num );
+if(num > 0)
+{
+print("the number is positive"\n);
+}
+else if(num==0)
+{
+printf("the  number is zero"\n);
+}
+else
+{
+printf("the number is negative"\n);
+}
+return 0;
+}
