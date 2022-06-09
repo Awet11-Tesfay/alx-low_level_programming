df80e2b015c1298306e5ba7da01147868ed2510e
Awet11_Tesfay <hailegb2021@gmail.com>
2022-06-08 17:40:00 -0700

    Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+*print_alphabet_x10-starting point
+*
+*Description:10xalphabet
+*Return:0x10 a-z
+*/
+void print_alphabet_x10(void)
+{
+int n, b;
+b = 0;
+while (b < 10)
+{
+for (n = 'a'; n <= 'z'; n++)
+{
+_putchar(n);
+}
+b++;
+_putchar('\n');
+}
+}
