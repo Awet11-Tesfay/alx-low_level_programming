79c25b5cc176262db79aac56fa2481517be4401d
Awet11_Tesfay <hailegb2021@gmail.com>
2022-06-08 17:27:16 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+*print_alphabet-Starting point
+*
+*Description:character display
+*Return:0 means succes
+*/
+void print_alphabet(void)
+{
+char al = 'a';
+while (al <= 'z')
+{
+_putchar(al);
+++al;
+}
+_putchar('\n');
+}
