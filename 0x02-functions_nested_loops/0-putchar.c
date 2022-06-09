0ed4380c738985bcfd0fd394ab2a1266ea77ad40
Awet11_Tesfay <hailegb2021@gmail.com>
2022-06-08 17:05:53 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,19 @@
-#include "main.h"
-/**                                                                            
-*main-Starting point
+#include"main.h"
+/**
+**main-Starting point
 *
 *Description:Function prototype
 *Return:Always 0(Sucsess)
-*/                                                               
-int main(void)                                                              
+*/
+int main(void)
 {
 char str[8] = "_putchar";
 int i = 0;
 while(i < 8)
-{	
-_putchar(str[i]);                                                               
+{
+_putchar(str[i]);
 ++i;
 }
-_putchar('\n');                                          
+_putchar('\n');
 return (0);
 }
