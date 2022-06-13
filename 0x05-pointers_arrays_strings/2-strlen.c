7dc6d3daf0dcad419ad412a8928f8b62d42f76a2
Awet11_Tesfay <hailegb2021@gmail.com>
2022-06-13 13:06:52 -0700

    Task 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,39 +1,20 @@
-#include "main.h"                                                                                                                                    
+#include "main.h"
 
-                                                                                                                                                     
 
-/**                                                                                                                                                  
+/**
+ *  _strlen - a function that returns the length of a string
+ *  @s: char input
+ *  Return: length of the input string
+ */
+int _strlen(char *s)
+{
+	 int i = 1, sum = 0;
+	 char pl = s[0];
 
- * _strlen-length of string                                                                                                                          
-
- * @s:a pointer to an int that will be changed                                                                                                       
-
- *                                                                                                                                                   
-
- * Return:void that means our answer is correct                                                                                                      
-
- */                                                                                                                                                  
-
-                                                                                                                                                     
-
-int _strlen(char *s)                                                                                                                                 
-
-{                                                                                                                                                    
-
-        int u;                                                                                                                                       
-
-                                                                                                                                                     
-
-        u = 0;                                                                                                                                       
-
-        while (s[u] != "\0")                                                                                                                         
-
-        {                                                                                                                                            
-
-                u++;                                                                                                                                 
-
-        }                                                                                                                                            
-
-        return (u);                                                                                                                                  
-
+	 while (pl != '\0')
+	 {
+		 sum++;
+		 pl = s[i++];
+	 }
+	 return (sum);
 }
