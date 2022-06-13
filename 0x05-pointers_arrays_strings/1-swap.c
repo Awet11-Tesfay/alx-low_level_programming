a99c3193a6e858cbebdd99ea22a6906a4e46e496
Awet11_Tesfay <hailegb2021@gmail.com>
2022-06-13 12:03:32 -0700

    Task 1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -3,11 +3,13 @@
 /**
  * swap_list-swap the value of  two integers
  * using two input parameters
+ *
  * @a:tnput parameter 1
  * @b:input parameter 2
  *
  * Return:Nothing
  */
+
 void swap_int(int *a, int *b)
 {
 	int temp;
