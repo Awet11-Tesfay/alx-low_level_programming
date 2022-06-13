cc31d1354dac70abcbb8b5ee6ef2285f23a125a4
Awet11_Tesfay <hailegb2021@gmail.com>
2022-06-13 16:08:01 -0700

    Task 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,18 +9,17 @@
  */
 void rev_string(char *s)
 {
-	char r = s[0];
-	int count = 0;
-	int j = 0;
-	
-	while (s[count] != '\0')
-		count++;
-	while (j < count)
-	{
-		count--;
-		r = s[j];
-		s[j] = s[count];
-		s[count] = r;
-		j++;
-	}
+char r = s[0];
+int count = 0;
+int j = 0;
+while (s[count] != '\0')
+count++;
+while (j < count)
+{
+count--;
+r = s[j];
+s[j] = s[count];
+s[count] = r;
+j++;
+}
 }
