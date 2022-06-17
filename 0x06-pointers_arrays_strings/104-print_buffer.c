820c1b87550fcb7e1b5c1575fb9d231b5ba2d8e0
Awet11_Tesfay <hailegb2021@gmail.com>
2022-06-17 09:25:05 -0700

    Task 12

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -17,25 +17,25 @@ void print_buffer(char *b, int size)
 	}
         for (i = 0; i < size; i += 10)
         {
-	printf("%08x: ", i);
-	for (j = i; j < i + 10; j += 2)
-	{
-		for (k = j; k <= j + 1; k++)
-			if ((b[k] != 0) && (k < size))
-				printf("%02x", b[k]);
-			else if (k < size)
-				printf("%02x", 0);
-			else
-				printf("  ");
-		printf(" ");
-	}
-	for (f = i; f < i + 10; f++)
-	{
-		if ((b[f] >= 32 && b[f] <= 126) && (f < size))
-			printf("%c", b[f]);
-		else if (f < size)
-			printf("%c", '.');
-	}
-	printf("\n");
-}
-}
+	         printf("%08x: ", i);
+	         for (j = i; j < i + 10; j += 2)
+	         {
+		          for (k = j; k <= j + 1; k++)
+			           if ((b[k] != 0) && (k < size))
+				           printf("%02x", b[k]);
+			           else if (k < size)
+				            printf("%02x", 0);
+			           else
+				            printf("  ");
+		           printf(" ");
+	          }
+	          for (f = i; f < i + 10; f++)
+	          {
+		           if ((b[f] >= 32 && b[f] <= 126) && (f < size))
+			           printf("%c", b[f]);
+		           else if (f < size)
+			           printf("%c", '.');
+	          }
+	          printf("\n"
+	 }
+}	 
